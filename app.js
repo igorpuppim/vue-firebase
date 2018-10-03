5bf7dd1ec8f65e11fa338682d280310c01f7fb45
Igor Alpoim O. Puppim <igor.puppim@gmail.com>
2018-10-03 12:17:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 //Criando a instancia do Vue e passando controle da div com id #app
-new app = new Vue({
+new Vue({
   el: '#app',
   data: {
-    title: 'Exercicio 1',
-    texto: 'teste 1'
+    title: 'Exercicio 4',
+	texto: 'aula 8',
+	musica: ' i miss you: ',
+	url: 'https://www.youtube.com',
+	classes: ['one','two']
+  },
+  methods: {
+	greet(param){
+		
+		return `${param} - ${this.musica} HELLO THERE, THE ANGEL FROM MY NIGHTMARE`
+	}  
   }
 })
